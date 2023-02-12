d0e0e6e4b089eac3121bf9e31c22761600b65e1e
OliverSchlueter <oliverhd.1303@gmail.com>
2023-02-12 13:10:37 +0100

    Add ability to change the background

--- src/main/java/de/oliver/fancyholograms/commands/HologramCommand.java
+++ src/main/java/de/oliver/fancyholograms/commands/HologramCommand.java
@@ -7,6 +7,7 @@ import de.oliver.fancyholograms.HologramManager;
 import de.oliver.fancyholograms.ReflectionHelper;
 import net.minecraft.command.argument.PosArgument;
 import net.minecraft.command.argument.TextArgumentType;
+import net.minecraft.command.argument.Vec3ArgumentType;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.decoration.DisplayEntity;
@@ -40,8 +41,13 @@ public class HologramCommand {
                                             )
                                         )
                                         .then(CommandManager.literal("position")
-                                                .then(CommandManager.argument("position", net.minecraft.command.argument.Vec3ArgumentType.vec3())
+                                                .then(CommandManager.argument("position", Vec3ArgumentType.vec3())
                                                         .executes(context -> executeEditPos(context.getSource(), context.getArgument("name", String.class), context.getArgument("position", PosArgument.class)))
+                                                )
+                                        )
+                                        .then(CommandManager.literal("background")
+                                                .then(CommandManager.argument("background", net.minecraft.command.argument.ColorArgumentType.color())
+                                                        .executes(context -> executeEditBackground(context.getSource(), context.getArgument("name", String.class), context.getArgument("background", Formatting.class)))
                                                 )
                                         )
                                 )
@@ -78,6 +84,13 @@ public class HologramCommand {
     private static int executeEditPos(ServerCommandSource source, String name, PosArgument pos){
         DisplayEntity.TextDisplayEntity entity = (DisplayEntity.TextDisplayEntity) HologramManager.getHologram(name);
         entity.setPosition(pos.toAbsolutePos(source));
+        source.getPlayer().sendMessage(Text.literal("Edited hologram").formatted(Formatting.GREEN));
+        return 1;
+    }
+
+    private static int executeEditBackground(ServerCommandSource source, String name, Formatting color){
+        DisplayEntity.TextDisplayEntity entity = (DisplayEntity.TextDisplayEntity) HologramManager.getHologram(name);
+        entity.getDataTracker().set((TrackedData<Integer>) ReflectionHelper.getStaticValue(DisplayEntity.TextDisplayEntity.class, "BACKGROUND"), color.getColorValue() | 0xC8000000);
         source.getPlayer().sendMessage(Text.literal("Edited hologram").formatted(Formatting.GREEN));
         return 1;
     }
