fd3aa7cc4dd2143588be1b4628d6faaee40f6c83
OliverSchlueter <oliverhd.1303@gmail.com>
2023-02-12 14:05:18 +0100

    Add ability to change the scale

--- src/main/java/de/oliver/fancyholograms/commands/HologramCommand.java
+++ src/main/java/de/oliver/fancyholograms/commands/HologramCommand.java
@@ -1,6 +1,7 @@
 package de.oliver.fancyholograms.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import de.oliver.fancyholograms.HologramManager;
@@ -17,6 +18,7 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import org.joml.Vector3f;
 
 import java.util.Arrays;
 
@@ -62,6 +64,11 @@ public class HologramCommand {
                                         .then(CommandManager.literal("billboard")
                                                 .then(CommandManager.argument("billboard", StringArgumentType.word()).suggests(BILLBOARD_SUGGESTION_PROVIDER)
                                                         .executes(context -> executeEditBillboard(context.getSource(), context.getArgument("name", String.class), context.getArgument("billboard", String.class)))
+                                                )
+                                        )
+                                        .then(CommandManager.literal("scale")
+                                                .then(CommandManager.argument("scale", FloatArgumentType.floatArg())
+                                                        .executes(context -> executeEditScale(context.getSource(), context.getArgument("name", String.class), context.getArgument("scale", Float.class)))
                                                 )
                                         )
                                 )
@@ -129,6 +136,13 @@ public class HologramCommand {
         }
 
         entity.getDataTracker().set((TrackedData<Byte>) ReflectionHelper.getStaticValue(DisplayEntity.class, "BILLBOARD"), index);
+        source.getPlayer().sendMessage(Text.literal("Edited hologram").formatted(Formatting.GREEN));
+        return 1;
+    }
+
+    private static int executeEditScale(ServerCommandSource source, String name, Float scale){
+        DisplayEntity entity = (DisplayEntity) HologramManager.getHologram(name);
+        entity.getDataTracker().set((TrackedData<Vector3f>) ReflectionHelper.getStaticValue(DisplayEntity.class, "SCALE"), new Vector3f(scale, scale, scale));
         source.getPlayer().sendMessage(Text.literal("Edited hologram").formatted(Formatting.GREEN));
         return 1;
     }
